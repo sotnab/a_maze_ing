668449887b5735fb2cb015f56d3c1a9dc32c56e5
Jaroslaw Zurek <contact@zurek.pro>
2026-08-30 01:25:50 +0200

    Generate 42 pattern in maze and present using MLX
    - add text representation of 42 pattern
    - selected cells are omitted during DFS generation
    - 42 cells are completely closed
    - entry and exit are safe to not be overlay by 42 pattern
    - add basic validation - e.g. too small maze

--- src/maze_generator/maze_gen.py
+++ src/maze_generator/maze_gen.py
@@ -14,40 +14,8 @@ from random import Random
 
 from .maze import Maze
 from .maze_grid import create_grid, grid_to_hex, open_wall
+from .pattern_42 import get_42_cells
 from src.maze_config import MazeConfig
-
-
-def get_unvisited_neighbours(
-    current: tuple[int, int],
-    visited: list[list[bool]],
-    width: int,
-    height: int,
-) -> list[tuple[int, int]]:
-    """Return unvisited neighbours of current cell."""
-    x, y = current
-    neighbours = []
-
-    # North
-    if y > 0:
-        if not visited[y - 1][x]:
-            neighbours.append((x, y - 1))
-
-    # East
-    if x < width - 1:
-        if not visited[y][x + 1]:
-            neighbours.append((x + 1, y))
-
-    # South
-    if y < height - 1:
-        if not visited[y + 1][x]:
-            neighbours.append((x, y + 1))
-
-    # West
-    if x > 0:
-        if not visited[y][x - 1]:
-            neighbours.append((x - 1, y))
-
-    return neighbours
 
 
 def create_visited(width: int, height: int) -> list[list[bool]]:
@@ -65,8 +33,57 @@ def create_visited(width: int, height: int) -> list[list[bool]]:
     return visited
 
 
+def get_unvisited_neighbours(
+    current: tuple[int, int],
+    visited: list[list[bool]],
+    blocked: list[tuple[int, int]],
+    width: int,
+    height: int,
+) -> list[tuple[int, int]]:
+    """Return unvisited and non-blocked neighbours."""
+    x, y = current
+    neighbours = []
+
+    # North
+    if y > 0:
+        next_cell = (x, y - 1)
+
+        if next_cell not in blocked:
+            if not visited[y - 1][x]:
+                neighbours.append(next_cell)
+
+    # East
+    if x < width - 1:
+        next_cell = (x + 1, y)
+
+        if next_cell not in blocked:
+            if not visited[y][x + 1]:
+                neighbours.append(next_cell)
+
+    # South
+    if y < height - 1:
+        next_cell = (x, y + 1)
+
+        if next_cell not in blocked:
+            if not visited[y + 1][x]:
+                neighbours.append(next_cell)
+
+    # West
+    if x > 0:
+        next_cell = (x - 1, y)
+
+        if next_cell not in blocked:
+            if not visited[y][x - 1]:
+                neighbours.append(next_cell)
+
+    return neighbours
+
+
 def generate_dfs(
-    grid: list[list[int]], start: tuple[int, int], random: Random
+    grid: list[list[int]],
+    start: tuple[int, int],
+    blocked: list[tuple[int, int]],
+    random: Random,
 ) -> None:
     """Generate a perfect maze using iterative DFS."""
     height = len(grid)
@@ -80,10 +97,12 @@ def generate_dfs(
     visited[start_y][start_x] = True
     stack.append(start)
 
-    while stack:
+    while len(stack) > 0:
         current = stack[-1]
 
-        neighbours = get_unvisited_neighbours(current, visited, width, height)
+        neighbours = get_unvisited_neighbours(
+            current, visited, blocked, width, height
+        )
 
         if len(neighbours) == 0:
             stack.pop()
@@ -107,7 +126,14 @@ class MazeGen:
     def generate(self) -> Maze:
         grid = create_grid(self.config.width, self.config.height)
 
-        generate_dfs(grid, self.config.entry, self.random)
+        blocked = get_42_cells(
+            self.config.width,
+            self.config.height,
+            self.config.entry,
+            self.config.exit,
+        )
+
+        generate_dfs(grid, self.config.entry, blocked, self.random)
 
         data = grid_to_hex(grid)
 
