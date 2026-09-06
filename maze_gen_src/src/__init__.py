d76a2d968ec7e8279d1c36c9c2e7de19ab748250
Wiktor Baran <wiktor@baran.hub.pl>
2026-09-06 16:21:26 +0200

    maze_gen package structure

--- maze_gen_src/src/__init__.py
+++ maze_gen_src/src/__init__.py
@@ -6,14 +6,14 @@
 #  By: wbaran <wbaran@student.42.fr>             +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/08/12 17:37:29 by wbaran          #+#    #+#               #
-#  Updated: 2026/09/06 15:33:09 by wbaran          ###   ########.fr        #
+#  Updated: 2026/09/06 16:19:20 by wbaran          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
-from .src.maze import Maze
-from .src.maze_gen import MazeGen
-from .src.config.maze_config import MazeConfig
-from .src.types import MazeData, Cell, Grid, Step
+from .maze import Maze
+from .maze_gen import MazeGen
+from .config.maze_config import MazeConfig
+from .types import MazeData, Cell, Grid, Step
 
 __all__ = [
     "Maze",
